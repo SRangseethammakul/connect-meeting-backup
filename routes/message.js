332211b3a8e73e31e489e7942cb0a68c74b6f733
RaSuttipong <RaSuttipong@chg.co.th>
2022-01-25 10:48:32 +0700

    change verify

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
 
-router.get('/', messageController.index);
+router.get('/', messageController.verifyWebHook);
 
 module.exports = router;
